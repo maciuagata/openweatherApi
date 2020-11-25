647f83790bb09728295c9d11090ef023eaaf901f
Agata <maciulewiczagata@gmail.com>
2020-11-25 19:41:01 +0200

    Add openweather api project

--- uzd2/apiConfig.php
+++ uzd2/apiConfig.php
@@ -1,5 +1,5 @@
 <?php
-const API_KEY = "ec697228a9833d323df8ba5bf710fb88";
+const API_KEY = "";
 const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
 const MODE = "xml";
 const UNITS  = "metric"; 
